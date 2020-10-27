36b40d5d4c2fbcfdc28a648cb930a6075fb11a12
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2020-10-27 20:48:00 +0100

    Create sn76489_stupid.py

---
+++ sn76489_stupid.py
@@ -0,0 +1,16 @@
+import sn76489
+
+def main():
+    global i2cAddr
+    global index1
+    pcfAddr = 0x23
+    index1 = 1
+    
+    sdef = SNdefs()
+
+
+print("APP start")
+main()
+print("APP eind")
+
+
